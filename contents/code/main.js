e8453b72804d32ae8cc6bdd3b0107f428305fb6f
Yanshuo Chu <yanshuochu@qq.com>
2023-05-10 21:41:09 -0500

    add switch to x function

--- contents/code/main.js
+++ contents/code/main.js
@@ -15,6 +15,18 @@ function switchDesktop() {
   return;
 }
 
+function switchDesktopX(x) {
+    if (workspace.currentDesktop == x) {
+        workspace.currentDesktop = lastDesktop;
+    } else {
+        workspace.currentDesktop = x
+    }
+    return;
+}
+
 /* Register shortcut */
 registerShortcut('Switch to last used desktop', "", "", function() { switchDesktop(); });
-
+for (let i = 1; i <= 9; i++) {
+    registerShortcut('Switch to last used desktopx ' + i,
+                     "", "", function() { switchDesktopX(i); });
+}
